e183562857b5636fb78c81de99b2de5a2641239a
M <>
2022-03-01 14:07:28 +0100

    ajout ihm

---
+++ app/src/main/java/com/example/gestion_rdv/ui/main/demande_de_rdv.java
@@ -0,0 +1,16 @@
+package com.example.gestion_rdv.ui.main;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.os.Bundle;
+
+import com.example.gestion_rdv.R;
+
+public class demande_de_rdv extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_demande_de_rdv);
+    }
+}
